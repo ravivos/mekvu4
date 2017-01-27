78337f588a1195e78a48634df40adc22f3f04b00
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-27 22:50:20 +0200

    almost finished code, no compi errors

--- Detector.java
+++ Detector.java
@@ -24,6 +24,7 @@ import org.apache.hadoop.fs.FileSystem;
 public class Detector {
 	// User local temp folder
 	private static final Path TEMP_PATH = new Path("temp");
+	private static final Path TEMP_PATH2 = new Path("temp2");
 
 	public static class TokenizerMapper	extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private final static IntWritable one = new IntWritable(1);
@@ -73,30 +74,50 @@ public class Detector {
 	public static class CuttingReducer extends Reducer<Text, IntWritable, Text, Text> {
 		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-			for(Text val : values) 
-				context.write(key, val);
+			Configuration configuration = context.getConfiguration();
+			int n = Integer.parseInt(configuration.get("n"));
+			String s = "";
+			int count = 0;
+			for(Text val : values) {
+				count++;
+				s += val.toString();
+				if(count==n)
+					break;
+			}
+				context.write(key, new Text(s));
 		}
 	}
 	//END
+	
+	/*
+	 * TODO:
+	 * we have
+	 * text1.txt topN
+	 * text2.txt topN
+	 * text3.txt topN
+	 * text4.txt topN
+	 * text5.txt topN
+	 * ...
+	 * we need
+	 * text1.txt text2.txt sum(inter(topN1,topN2))
+	 */
 
-	public static class SwapMapper extends Mapper<Text, Text, IntWritable, Text> {
-		private final IntWritable count = new IntWritable();
+	public static class SwapMapper extends Mapper<Text, Text, Text, Text> {
 
 		public void map(Text key, Text value, Context context)
 				throws IOException, InterruptedException {
 
-			count.set(Integer.parseInt(value.toString()));
-			context.write(count, key);
+			context.write(value, key);
 		}
 	}
 
-	public static class OutputReducer extends Reducer<IntWritable, Text, Text, NullWritable> {
+	public static class OutputReducer extends Reducer<Text, Text, Text, Text> {
 		private final static NullWritable nothing = NullWritable.get();
 
-		public void reduce(IntWritable key, Iterable<Text> values, Context context)
+		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
 			for (Text val : values) {
-				context.write(val, nothing);
+				context.write(val, key);
 			}
 		}
 	}
@@ -104,6 +125,8 @@ public class Detector {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		FileSystem fs = FileSystem.get(conf);
+		conf.set("n", args[0]);
+        conf.set("k", args[1]);
 
 		// Just to be safe: clean temporary files before we begin
 		fs.delete(TEMP_PATH, true);
@@ -113,6 +136,10 @@ public class Detector {
 
 		// Setup first MapReduce phase
 		//this MapReduce will take all the files and make a map of word to counter
+		
+		/*
+		 * gets some files and maps file and word to occurence num
+		 */
 		System.out.println("Hello");
 		Job job1 = Job.getInstance(conf, "Detector-first");
 		job1.setJarByClass(Detector.class);
@@ -122,7 +149,7 @@ public class Detector {
 		job1.setMapOutputValueClass(IntWritable.class);
 		job1.setOutputKeyClass(Text.class);
 		job1.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job1, new Path(args[0]));
+		FileInputFormat.addInputPath(job1, new Path(args[2]));
 		FileOutputFormat.setOutputPath(job1, TEMP_PATH);
 		//FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 		System.out.println("Hello1");
@@ -130,6 +157,36 @@ public class Detector {
 		if(!status1) {
 			System.exit(1);
 		}
+		
+		
+		/*
+		 * map file to tuple of word and occurences
+		 * 
+		 */
+		// Setup second MapReduce phase
+				System.out.println("Hello2");
+				Job job3 = Job.getInstance(conf, "Detector-second");
+				job3.setJarByClass(Detector.class);
+				job3.setMapperClass(SwapMapper.class);
+				job3.setReducerClass(OutputReducer.class);
+				job3.setMapOutputKeyClass(IntWritable.class);
+				job3.setMapOutputValueClass(Text.class);
+				job3.setOutputKeyClass(Text.class);
+				job3.setOutputValueClass(NullWritable.class);
+				job3.setInputFormatClass(KeyValueTextInputFormat.class);
+				FileInputFormat.addInputPath(job3, TEMP_PATH);
+				FileOutputFormat.setOutputPath(job3, TEMP_PATH2);
+				System.out.println("Hello3");
+				boolean status3 = job3.waitForCompletion(true);
+				System.out.println("Hello4");
+
+
+				if (!status3) System.exit(1);
+				
+		/*
+		 * maps file to list of words and occurences
+		 * reduce to only first N (which will be sorted because of the prev map red
+		 */
 		// Setup second MapReduce phase
 		System.out.println("Hello2");
 		Job job2 = Job.getInstance(conf, "Detector-second");
@@ -142,13 +199,24 @@ public class Detector {
 		job2.setOutputValueClass(NullWritable.class);
 		job2.setInputFormatClass(KeyValueTextInputFormat.class);
 		FileInputFormat.addInputPath(job2, TEMP_PATH);
-		FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job2, new Path(args[3]));
 		System.out.println("Hello3");
 		boolean status2 = job2.waitForCompletion(true);
 		System.out.println("Hello4");
-		fs.delete(TEMP_PATH, true);
 
 		if (!status2) System.exit(1);
+		
+		/*
+		 * do cartesian on the prev map reduce keys and intersect the vals
+		 * remove (b,a) if we have (a,b)
+		 * 
+		 */
+		
+		
+		fs.delete(TEMP_PATH, true);
+		fs.delete(TEMP_PATH2, true);
+		
+	
 	}
 	// Clean temporary files from the first MapReduce phase
 
