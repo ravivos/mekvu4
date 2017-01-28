3d60f47639fec2c4638bb3274d9f53f2f7b31e08
Raviv Rachmiel <raviv.rachmiel@gmail.com>
2017-01-28 20:42:25 +0200

    mekvu-all-v1

--- Detector.java
+++ Detector.java
@@ -147,9 +147,9 @@ public class Detector {
 			while (st.hasMoreTokens()) {
 				String temp = key.toString();
 				String nt = st.nextToken();
-				String nothing = st.nextToken();
-				word.set(nt.split(" ")[0]); // word
-				context.write(word, new Text(temp + " " + nt));
+				String num = st.nextToken();
+				word.set(nt); // word
+				context.write(word, new Text(temp + " " + num));
 			}
 		}
 	}
